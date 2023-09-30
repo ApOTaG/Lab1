d49080362f5d7c9a1d55c4824c28c4c178f66182
ApOTaG <andrewanapoliskiy@gmail.com>
2023-09-30 16:26:53 +0300

    Added Second Button. Added Change Text Color functionality

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -15,10 +16,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         this.MainText = findViewById(R.id.MainText);
     }
 
     public void ChangeButtonClick(View view) {
         this.MainText.setText("Hello! :)");
+    }
+
+    public void ColorBtnClick(View view) {
+        MainText.setTextColor(Color.rgb(200,0,0));
     }
 }
