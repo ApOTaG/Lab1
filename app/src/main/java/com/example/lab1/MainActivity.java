d9aa1f75662edbbd59f100efcf65395a31f4d5db
ApOTaG <andrewanapoliskiy@gmail.com>
2023-09-30 16:11:19 +0300

    Added Button. Added Change Text functionality

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.lab1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView MainText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.MainText = findViewById(R.id.MainText);
+    }
+
+    public void ChangeButtonClick(View view) {
+        this.MainText.setText("Hello! :)");
     }
 }
